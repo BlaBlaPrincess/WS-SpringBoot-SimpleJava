f2b1715c6d55ea7293ad8921faef613febd4cde4
Alexander Simakin <qq.siiimka@gmail.com>
2021-06-23 00:28:35 +1000

    Changed ArrayCountingAlgorithmsPresenter output string

--- src/main/java/com/blablaprincess/springboot_simplejava/business/arraycounting/presenters/ArrayCountingAlgorithmsPresenter.java
+++ src/main/java/com/blablaprincess/springboot_simplejava/business/arraycounting/presenters/ArrayCountingAlgorithmsPresenter.java
@@ -5,6 +5,7 @@ import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -19,7 +20,14 @@ public class ArrayCountingAlgorithmsPresenter<T> {
 
     public String Present(T[] array) {
         StringBuilder result = new StringBuilder();
-        result.append(String.format("Counting algorithms: %d", algorithms.size()));
+        result.append(String.format("Counting algorithms for %s", array.getClass().getSimpleName()));
+
+        if (algorithms.size() == 0) {
+            result.append(" not found");
+            return result.toString();
+        }
+
+        result.append(String.format(": %d%n%nArray: %s%n", algorithms.size(), Arrays.toString(array)));
         for (var alg : algorithms) {
             result.append(String.format("%n%s: %.2f", alg.getClass().getSimpleName(), alg.count(array)));
         }
