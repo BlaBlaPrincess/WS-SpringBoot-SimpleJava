9edfe905aefead71b95d40caea778aa35902e8eb
Alexander Simakin <qq.siiimka@gmail.com>
2021-06-25 22:34:47 +1000

    Add test app context profile

--- src/main/java/com/blablaprincess/springboot_simplejava/runners/CommandLineRunnerImpl.java
+++ src/main/java/com/blablaprincess/springboot_simplejava/runners/CommandLineRunnerImpl.java
@@ -5,10 +5,12 @@ import com.blablaprincess.springboot_simplejava.business.digitsrepresentation.Di
 import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
+@Profile("!test")
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
 
