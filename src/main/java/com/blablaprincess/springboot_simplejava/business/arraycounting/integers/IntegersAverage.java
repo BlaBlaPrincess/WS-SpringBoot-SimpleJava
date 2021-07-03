2172624657cb944a79e9bfdb75e2322f21f700d9
Alexander Simakin <qq.siiimka@gmail.com>
2021-07-03 14:36:15 +1000

    Remove redundant var from IntegersAverage

--- src/main/java/com/blablaprincess/springboot_simplejava/business/arraycounting/integers/IntegersAverage.java
+++ src/main/java/com/blablaprincess/springboot_simplejava/business/arraycounting/integers/IntegersAverage.java
@@ -13,13 +13,12 @@ public class IntegersAverage implements ArrayCountingAlgorithm<Integer> {
 
     @Override
     public double count(Integer[] array) {
-        var count = array.length;
-        if (count == 0) {
+        if (array.length == 0) {
             throw new ArithmeticException();
         }
         return (double) Arrays.stream(array)
                               .mapToInt(Integer::intValue)
-                              .sum() / count;
+                              .sum() / array.length;
     }
 
 }
