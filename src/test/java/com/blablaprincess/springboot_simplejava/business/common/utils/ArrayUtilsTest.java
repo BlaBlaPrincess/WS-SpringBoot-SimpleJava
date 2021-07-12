c86515bc1740d35f0c94d5b46d2b35bab89d4234
Alexander Simakin <qq.siiimka@gmail.com>
2021-07-12 14:09:45 +1000

    Add ArrayUtils test

---
+++ src/test/java/com/blablaprincess/springboot_simplejava/business/common/utils/ArrayUtilsTest.java
@@ -0,0 +1,47 @@
+package com.blablaprincess.springboot_simplejava.business.common.utils;
+
+import com.blablaprincess.springboot_simplejava.business.common.exceptions.EmptyArrayException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class ArrayUtilsTest {
+
+    private interface TestScenario {
+        void test(Object[] array, Object expected);
+    }
+
+    private static final TestScenario toDoesNotThrows = (array, expected) -> {
+        // Act + Assert
+        assertDoesNotThrow(() -> ArrayUtils.ValidateArrayNotEmpty(array));
+    };
+
+    @SuppressWarnings("unchecked")
+    private static final TestScenario toThrows = (array, exception) -> {
+        // Act + Assert
+        assertThrows((Class<Exception>) exception, () -> ArrayUtils.ValidateArrayNotEmpty(array));
+    };
+
+    @DisplayName("validateArrayNotEmpty")
+    @ParameterizedTest(name = "with {0}")
+    @MethodSource("getValidateArrayNotEmptyCases")
+    <T> void validateArrayNotEmpty(String description, Object[] array, TestScenario testScenario, Object expected) {
+        testScenario.test(array, expected);
+    }
+
+    static Stream<Arguments> getValidateArrayNotEmptyCases() {
+        return Stream.of(
+                arguments("empty array", new Integer[]   {},              toThrows,        EmptyArrayException.class),
+                arguments("char array",  new Character[] {'1', '2', '3'}, toDoesNotThrows, null),
+                arguments("num array",   new Integer[]   {1, 2, 3},       toDoesNotThrows, null)
+                        );
+    }
+
+}
