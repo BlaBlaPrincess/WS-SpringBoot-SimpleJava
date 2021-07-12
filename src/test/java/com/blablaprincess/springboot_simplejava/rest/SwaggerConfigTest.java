1b323ad88508f131b8a987913b7b2261e0ad6b68
Alexander Simakin <qq.siiimka@gmail.com>
2021-07-12 15:18:22 +1000

    Add SwaggerConfig test

---
+++ src/test/java/com/blablaprincess/springboot_simplejava/rest/SwaggerConfigTest.java
@@ -0,0 +1,28 @@
+package com.blablaprincess.springboot_simplejava.rest;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SwaggerConfig.class)
+class SwaggerConfigTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @SneakyThrows
+    @DisplayName("GET /swagger-ui.html")
+    void get() {
+        // Act + Assert
+        mvc.perform(MockMvcRequestBuilders.get("/swagger-ui.html"))
+           .andExpect(status().isOk());
+    }
+
+}
