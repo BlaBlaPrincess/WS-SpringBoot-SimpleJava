3f08ac3310a18eade8840dfdcbf1dcce1d60e78e
Alexander Simakin <qq.siiimka@gmail.com>
2021-07-12 23:38:10 +1000

    Refactor Controller to CountsController and IndexController

---
+++ src/main/java/com/blablaprincess/springboot_simplejava/rest/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package com.blablaprincess.springboot_simplejava.rest.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.PERMANENT_REDIRECT)
+    @Operation(summary = "redirects index to swagger ui")
+    public RedirectView redirectIndexToSwaggerUi() {
+        return new RedirectView("/swagger-ui.html");
+    }
+
+}
