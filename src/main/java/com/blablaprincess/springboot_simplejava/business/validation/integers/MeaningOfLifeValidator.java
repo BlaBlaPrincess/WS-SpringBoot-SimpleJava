10d73cb6fcf6253b4f3697f20d439a65282153d8
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-07-22 10:18:41 +0300

    Add MeaningOfLifeValidator

---
+++ src/main/java/com/blablaprincess/springboot_simplejava/business/validation/integers/MeaningOfLifeValidator.java
@@ -0,0 +1,22 @@
+package com.blablaprincess.springboot_simplejava.business.validation.integers;
+
+import com.blablaprincess.springboot_simplejava.business.arraycounting.integers.IntegersSum;
+import com.blablaprincess.springboot_simplejava.business.digitsrepresentation.DigitsRepresentation;
+import com.blablaprincess.springboot_simplejava.business.validation.ValidationException;
+import com.blablaprincess.springboot_simplejava.business.validation.Validator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name = "validation.int.meaning-of-life.enabled", havingValue = "true")
+public class MeaningOfLifeValidator implements Validator<Integer> {
+
+    @Override
+    public void validate(Integer value) throws ValidationException {
+        Integer[] array = DigitsRepresentation.getDigitsArray(value);
+        if (new IntegersSum().count(array) == 42) {
+            throw new ValidationException();
+        }
+    }
+
+}
