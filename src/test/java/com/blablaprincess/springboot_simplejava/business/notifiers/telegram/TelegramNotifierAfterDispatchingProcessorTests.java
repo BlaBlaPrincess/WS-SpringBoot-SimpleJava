56663590cc2997358efcce0cf2d0bef7aeb37fca
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-07 07:39:54 +0300

    Add TelegramBotNotifierAfterDispatchingProcessor

---
+++ src/test/java/com/blablaprincess/springboot_simplejava/business/notifiers/telegram/TelegramNotifierAfterDispatchingProcessorTests.java
@@ -0,0 +1,27 @@
+package com.blablaprincess.springboot_simplejava.business.notifiers.telegram;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.mockito.Mockito.*;
+
+class TelegramNotifierAfterDispatchingProcessorTests {
+
+    private final TelegramBotNotifierService service = mock(TelegramBotNotifierService.class);
+
+    private final TelegramBotNotifierAfterDispatchingProcessor telegramBotNotifierAfterDispatchingProcessor
+            = new TelegramBotNotifierAfterDispatchingProcessor(service, new ObjectMapper());
+
+    @Test
+    void process() {
+        // Act
+        telegramBotNotifierAfterDispatchingProcessor.process("", mock(HttpServletRequest.class), mock(HttpServletResponse.class));
+
+        // Assert
+        verify(service, times(1)).sendNotification(anyString());
+    }
+
+}
