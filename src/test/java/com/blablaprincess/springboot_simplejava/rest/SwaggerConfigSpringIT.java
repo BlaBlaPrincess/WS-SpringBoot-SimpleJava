fc2f74ead4441ad6a6501a54413e8b675b1d7777
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-07 07:48:05 +0300

    Merge (a93406c1) fix

--- src/test/java/com/blablaprincess/springboot_simplejava/rest/SwaggerConfigSpringIT.java
+++ src/test/java/com/blablaprincess/springboot_simplejava/rest/SwaggerConfigSpringIT.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Execution(ExecutionMode.SAME_THREAD)
 @SpringBootTest
 @AutoConfigureMockMvc
-class SwaggerConfigMvcIT {
+class SwaggerConfigSpringIT {
 
     @Autowired
     private MockMvc mvc;
