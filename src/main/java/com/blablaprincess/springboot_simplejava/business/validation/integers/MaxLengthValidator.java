5c1c35854bc504ff3e6357bcc53636c57b0601ed
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-14 02:20:56 +1000

    Remove redundant annotations from MaxLengthValidator

--- src/main/java/com/blablaprincess/springboot_simplejava/business/validation/integers/MaxLengthValidator.java
+++ src/main/java/com/blablaprincess/springboot_simplejava/business/validation/integers/MaxLengthValidator.java
@@ -6,26 +6,23 @@ import com.blablaprincess.springboot_simplejava.business.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
-@PropertySource("classpath:application.properties")
 @ConditionalOnProperty(name = "validation.int.max-length.enabled", havingValue = "true")
 public class MaxLengthValidator implements Validator<Integer> {
 
-    @Value("${validation.int.max-length:8}")
-    private final Integer MAX_LENGTH;
+    private final Integer maxLength;
 
     @Autowired
     public MaxLengthValidator(@Value("${validation.int.max-length:8}") int maxLength) {
-        MAX_LENGTH = maxLength;
+        this.maxLength = maxLength;
     }
 
     @Override
     public void validate(Integer value) throws ValidationException {
-        if (IntUtils.getLength(value) > MAX_LENGTH) {
-            throw new ValidationException(String.format("The number cannot be larger than %d signs", MAX_LENGTH));
+        if (IntUtils.getLength(value) > maxLength) {
+            throw new ValidationException(String.format("The number cannot be larger than %d signs", maxLength));
         }
     }
 
