44b895d032a0a59311118baf08785cf22ddd9cd8
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-13 18:58:47 +1000

    Refactor ControllerCallsHistoryService and ControllerCallsHistoryController with querydsl

--- src/main/java/com/blablaprincess/springboot_simplejava/rest/controllers/ControllerCallsHistoryController.java
+++ src/main/java/com/blablaprincess/springboot_simplejava/rest/controllers/ControllerCallsHistoryController.java
@@ -1,17 +1,13 @@
 package com.blablaprincess.springboot_simplejava.rest.controllers;
 
 import com.blablaprincess.springboot_simplejava.business.controllercalls.ControllerCallDescriptionEntity;
-import com.blablaprincess.springboot_simplejava.rest.actions.ControllerCallsHistoryAction;
+import com.blablaprincess.springboot_simplejava.business.controllercalls.ControllerCallsHistoryLastCallsArgs;
+import com.blablaprincess.springboot_simplejava.business.controllercalls.ControllerCallsHistoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.convert.DurationFormat;
-import org.springframework.boot.convert.DurationStyle;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -20,31 +16,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ControllerCallsHistoryController {
 
-    private final ControllerCallsHistoryAction controllerCallsHistoryAction;
-    private final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private final ControllerCallsHistoryService controllerCallsHistoryService;
 
     @GetMapping("")
-    @Operation(summary = "get list of all controller calls")
-    public List<ControllerCallDescriptionEntity> getControllerCalls(Integer amount) {
-        return controllerCallsHistoryAction.getCalls(amount);
-    }
-
-    @GetMapping("/from")
     @Operation(summary = "get list of controller calls by specific params")
-    public List<ControllerCallDescriptionEntity> getControllerCalls
-            (Integer amount,
-             @RequestParam(required = true) @DurationFormat(DurationStyle.SIMPLE) Duration offset,
-             @RequestParam(required = false) @DateTimeFormat(pattern = DATE_TIME_PATTERN) LocalDateTime from) {
-        return controllerCallsHistoryAction.getCalls(amount, offset, from);
-    }
-
-    @GetMapping("/between")
-    @Operation(summary = "get list of controller calls by specific params")
-    public List<ControllerCallDescriptionEntity> getControllerCalls
-            (Integer amount,
-             @RequestParam(required = true) @DateTimeFormat(pattern = DATE_TIME_PATTERN) LocalDateTime from,
-             @RequestParam(required = false) @DateTimeFormat(pattern = DATE_TIME_PATTERN) LocalDateTime to) {
-        return controllerCallsHistoryAction.getCalls(amount, from, to);
+    public List<ControllerCallDescriptionEntity> getControllerCalls(ControllerCallsHistoryLastCallsArgs args) {
+        return controllerCallsHistoryService.getLastCalls(args);
     }
 
 }
--- src/test/java/com/blablaprincess/springboot_simplejava/rest/controllers/ControllerCallsHistoryControllerMvcIT.java
+++ src/test/java/com/blablaprincess/springboot_simplejava/rest/controllers/ControllerCallsHistoryControllerMvcIT.java
@@ -1,6 +1,7 @@
 package com.blablaprincess.springboot_simplejava.rest.controllers;
 
-import com.blablaprincess.springboot_simplejava.rest.actions.ControllerCallsHistoryAction;
+import com.blablaprincess.springboot_simplejava.business.controllercalls.ControllerCallsHistoryLastCallsArgs;
+import com.blablaprincess.springboot_simplejava.business.controllercalls.ControllerCallsHistoryService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
@@ -13,7 +14,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
@@ -29,12 +29,12 @@ class ControllerCallsHistoryControllerMvcIT {
     private MockMvc mvc;
 
     @MockBean
-    private ControllerCallsHistoryAction controllerCallsHistoryAction;
+    private ControllerCallsHistoryService controllerCallsHistoryService;
 
     @DisplayName("GET /calls")
     @ParameterizedTest(name = "/calls{0}")
-    @MethodSource("getIndexCases")
-    void getControllerCalls(String urlParams, Integer expectedAmount) throws Exception {
+    @MethodSource("getCallsCases")
+    void getControllerCalls(String urlParams, ControllerCallsHistoryLastCallsArgs expected) throws Exception {
         // Arrange
         String urlBase = "/calls";
 
@@ -43,68 +43,26 @@ class ControllerCallsHistoryControllerMvcIT {
                 // Assert
                 .andExpect(status().isOk());
 
-        verify(controllerCallsHistoryAction).getCalls(eq(expectedAmount));
+        verify(controllerCallsHistoryService).getLastCalls(eq(expected));
     }
 
-    @DisplayName("GET /calls/from")
-    @ParameterizedTest(name = "/calls/from{0}")
-    @MethodSource("getFromCases")
-    void getControllerCalls
-            (String urlParams, Integer expectedAmount, Duration expectedDuration, LocalDateTime expectedFrom) throws Exception {
-        // Arrange
-        String urlBase = "/calls/from";
-
-        // Act
-        mvc.perform(MockMvcRequestBuilders.get(urlBase + urlParams))
-                // Assert
-                .andExpect(status().isOk());
-
-        verify(controllerCallsHistoryAction).getCalls(eq(expectedAmount), eq(expectedDuration), eq(expectedFrom));
-    }
-
-    @DisplayName("GET /calls/between")
-    @ParameterizedTest(name = "/calls/between{0}")
-    @MethodSource("getBetweenCases")
-    void getControllerCalls
-            (String urlParams, Integer expectedAmount, LocalDateTime expectedFrom, LocalDateTime expectedTo) throws Exception {
-        // Arrange
-        String urlBase = "/calls/between";
-
-        // Act
-        mvc.perform(MockMvcRequestBuilders.get(urlBase + urlParams))
-                // Assert
-                .andExpect(status().isOk());
-
-        verify(controllerCallsHistoryAction).getCalls(eq(expectedAmount), eq(expectedFrom), eq(expectedTo));
-    }
-
-    static Stream<Arguments> getIndexCases() {
+    static Stream<Arguments> getCallsCases() {
+        LocalDateTime to   = LocalDateTime.parse("2021-01-01T12:00");
+        LocalDateTime from = LocalDateTime.parse("2011-01-01T12:00");
         return Stream.of(
-                arguments("",            null),
-                arguments("?amount=100", 100)
+                arguments("",
+                        argsBuilder().build()),
+                arguments("?timestampBefore=2021-01-01 12:00",
+                        argsBuilder().timestampBefore(to).build()),
+                arguments("?timestampAfter=2011-01-01 12:00",
+                        argsBuilder().timestampAfter(from).build()),
+                arguments("?timestampBefore=2021-01-01 12:00&timestampAfter=2011-01-01 12:00",
+                        argsBuilder().timestampBefore(to).timestampAfter(from).build())
         );
     }
 
-    static Stream<Arguments> getFromCases() {
-        return Stream.of(
-                arguments("?offset=PT1S",           null, Duration.parse("PT1S"),         null),
-                arguments("?offset=PT-10H",         null, Duration.parse("PT-10H"),       null),
-                arguments("?offset=P1000DT10H1S",   null, Duration.parse("P1000DT10H1S"), null),
-                arguments("?amount=10&offset=PT1S", 10,   Duration.parse("PT1S"),         null),
-                arguments("?amount=10&offset=PT1S&from=2021-01-01 12:00:00",
-                        10, Duration.parse("PT1S"), LocalDateTime.parse("2021-01-01T12:00"))
-        );
-    }
-
-    static Stream<Arguments> getBetweenCases() {
-        return Stream.of(
-                arguments("?from=2021-01-01 12:00:00",
-                        null, LocalDateTime.parse("2021-01-01T12:00"), null),
-                arguments("?from=2021-01-01 12:00:00&to=2021-11-11 12:00:00",
-                        null, LocalDateTime.parse("2021-01-01T12:00"), LocalDateTime.parse("2021-11-11T12:00")),
-                arguments("?amount=10&from=2021-01-01 12:00:00&to=2021-11-11 12:00:00",
-                        10, LocalDateTime.parse("2021-01-01T12:00"), LocalDateTime.parse("2021-11-11T12:00"))
-        );
+    private static ControllerCallsHistoryLastCallsArgs.ControllerCallsHistoryLastCallsArgsBuilder argsBuilder() {
+        return ControllerCallsHistoryLastCallsArgs.builder();
     }
 
 }
