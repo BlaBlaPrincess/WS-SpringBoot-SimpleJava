2b5d0c164f63ce9465c6c40a3fc66a2c51edd1ea
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-13 21:44:40 +1000

    Move AsyncConfig and WebClientConfig to configuration package

--- src/main/java/com/blablaprincess/springboot_simplejava/configuration/WebClientConfig.java
+++ src/main/java/com/blablaprincess/springboot_simplejava/configuration/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.blablaprincess.springboot_simplejava;
+package com.blablaprincess.springboot_simplejava.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
