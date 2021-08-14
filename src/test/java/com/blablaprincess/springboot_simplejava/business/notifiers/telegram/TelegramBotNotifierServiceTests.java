9e788103c83dd0cdf84183f560d8cac1364345c5
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-14 16:48:18 +1000

    Add uri equals asserting for TelegramBotNotifierServiceTests

--- src/test/java/com/blablaprincess/springboot_simplejava/business/notifiers/telegram/TelegramBotNotifierServiceTests.java
+++ src/test/java/com/blablaprincess/springboot_simplejava/business/notifiers/telegram/TelegramBotNotifierServiceTests.java
@@ -6,36 +6,46 @@ import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 class TelegramBotNotifierServiceTests {
 
-    private final TelegramBotNotifierService service;
-    private final ExchangeFunction exchangeFunction = mock(ExchangeFunction.class);
-
-    TelegramBotNotifierServiceTests() {
-        WebClient webClient = WebClient.builder()
-                .exchangeFunction(exchangeFunction)
-                .build();
-
-        service = new TelegramBotNotifierService("token", "chatId", webClient);
-    }
-
     @Test
+    @SuppressWarnings("Convert2Lambda")
     void sendNotification() {
         // Arrange
-        String message = "message";
-        when(exchangeFunction.exchange(any(ClientRequest.class)))
-                .thenReturn(Mono.just(ClientResponse.create(HttpStatus.OK).build()));
+        String message = "123abc\n!@#$%^&*()-+=\n\uD83D\uDC35\uD83D\uDE48\uD83D\uDE49️";
+        URI expectedUri = UriComponentsBuilder
+                .fromHttpUrl("https://api.telegram.org/bottoken/sendMessage?chat_id=chatId&text={message}")
+                .buildAndExpand(message)
+                .toUri();
 
-        // Act
+        ExchangeFunction exchangeFunction = new ExchangeFunction() {
+            public Mono<ClientResponse> exchange(ClientRequest request) {
+                URI actualUri = request.url();
+                assertEquals(expectedUri, actualUri);
+                return Mono.just(ClientResponse.create(HttpStatus.OK).build());
+            }
+        };
+        ExchangeFunction spyExchangeFunction = spy(exchangeFunction);
+
+        WebClient webClient = WebClient.builder()
+                .exchangeFunction(spyExchangeFunction)
+                .build();
+
+        TelegramBotNotifierService service = new TelegramBotNotifierService("token", "chatId", webClient);
+
+        // Act + Assert
         service.sendNotification(message);
-
-        // Assert
-        verify(exchangeFunction).exchange(any());
+        verify(spyExchangeFunction).exchange(any());
     }
 
 }
