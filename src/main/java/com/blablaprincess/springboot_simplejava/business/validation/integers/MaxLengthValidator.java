b73b9b550a2129d5aff9ba356ac8f98a5d39e13d
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-14 17:01:43 +1000

    Simplify MaxLengthValidator constructor with lombok

--- src/main/java/com/blablaprincess/springboot_simplejava/business/validation/integers/MaxLengthValidator.java
+++ src/main/java/com/blablaprincess/springboot_simplejava/business/validation/integers/MaxLengthValidator.java
@@ -3,21 +3,18 @@ package com.blablaprincess.springboot_simplejava.business.validation.integers;
 import com.blablaprincess.springboot_simplejava.business.common.utils.IntUtils;
 import com.blablaprincess.springboot_simplejava.business.validation.ValidationException;
 import com.blablaprincess.springboot_simplejava.business.validation.Validator;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConditionalOnProperty(name = "validation.int.max-length.enabled", havingValue = "true")
+@RequiredArgsConstructor
 public class MaxLengthValidator implements Validator<Integer> {
 
+    @Value("${validation.int.max-length:8}")
     private final Integer maxLength;
-
-    @Autowired
-    public MaxLengthValidator(@Value("${validation.int.max-length:8}") int maxLength) {
-        this.maxLength = maxLength;
-    }
 
     @Override
     public void validate(Integer value) throws ValidationException {
