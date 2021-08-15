79655f44ddf23e27db276351cd16a52b77838e82
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-15 19:55:56 +1000

    ProcessAfterDispatchingWith -> ProcessAfterHandlingWith: filter -> aspectj

--- src/main/java/com/blablaprincess/springboot_simplejava/rest/afterdispatchingprocessor/AfterDispatchingProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.blablaprincess.springboot_simplejava.rest.afterdispatchingprocessor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface AfterDispatchingProcessor {
-    void process(String responseBody, HttpServletRequest request, HttpServletResponse response);
-}
