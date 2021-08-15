af898a8942116b412a243c3d59b1a4afd5617708
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-15 19:56:25 +1000

    TelegramBotNotifierAfterDispatchingProcessor -> TelegramBotNotifierAfterHandlingProcessor

--- src/main/java/com/blablaprincess/springboot_simplejava/business/notifiers/telegram/adp/messagebuilder/TelegramBotNotifierAfterDispatchingProcessorMessageBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.blablaprincess.springboot_simplejava.business.notifiers.telegram.adp.messagebuilder;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface TelegramBotNotifierAfterDispatchingProcessorMessageBuilder {
-    String build(String responseBody, HttpServletRequest request, HttpServletResponse response);
-}
---
+++ src/main/java/com/blablaprincess/springboot_simplejava/business/notifiers/telegram/ahp/TelegramBotNotifierAfterHandlingProcessor.java
@@ -0,0 +1,30 @@
+package com.blablaprincess.springboot_simplejava.business.notifiers.telegram.ahp;
+
+import com.blablaprincess.springboot_simplejava.business.notifiers.telegram.TelegramBotNotifierService;
+import com.blablaprincess.springboot_simplejava.business.notifiers.telegram.ahp.messagebuilder.TelegramBotNotifierAfterHandlingProcessorMessageBuilder;
+import com.blablaprincess.springboot_simplejava.business.afterhandlingprocessor.AfterHandlingProcessor;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.Signature;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "telegram-bot.ahp.enabled", havingValue = "true")
+public class TelegramBotNotifierAfterHandlingProcessor implements AfterHandlingProcessor {
+
+    private final TelegramBotNotifierService notifierService;
+    private final TelegramBotNotifierAfterHandlingProcessorMessageBuilder messageBuilder;
+
+    @Override
+    public void processOnSuccess(Object[] args, Signature signature, Object response) {
+        String message = messageBuilder.buildOnSuccess(args, signature, response);
+        notifierService.sendNotification(message);
+    }
+
+    @Override
+    public void processOnThrows(Object[] args, Signature signature, Throwable throwable) {
+        String message = messageBuilder.buildOnThrows(args, signature, throwable);
+        notifierService.sendNotification(message);
+    }
+}
